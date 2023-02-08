c9828bd6dc63708ca19a699213ce4a144087b56d
Glaucio Almeida <glaucio@mail.com>
2023-02-08 20:26:02 -0300

    Navbar up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,24 @@
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 
 export default function Navbar() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="primary" sx={{borderTop: '3px solid #2b4ee9'}}>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        color="primary"
+        sx={{ borderTop: "3px solid #2b4ee9" }}
+      >
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Painel de Recebimento / Local: Centro de distribuição / Dia: 22/10/2023
+            Painel de Recebimento / Local: Centro de distribuição / Dia:
+            22/10/2023
           </Typography>
         </Toolbar>
+        <IconButton color="primary">
+          <FormatListBulletedIcon fontSize="large" />
+        </IconButton>
       </AppBar>
     </Box>
-     );
-  }
-  
-  
+  );
+}
