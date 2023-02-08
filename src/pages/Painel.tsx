5f2b0babf4b7db3e88fa86aef0fb77e5abcc9801
Glaucio Almeida <glaucio@mail.com>
2023-02-08 13:30:06 -0300

    change to router model

---
+++ src/pages/Painel.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Painel() {
+  return (
+      <div>Hi!</div>
+   );
+}
+
---
+++ src/router.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Home from "./pages/Home";
+import Painel from "./pages/Painel";
+import NotFound from "./pages/NotFound";
+
+//import { Route, Routes } from "react-router";
+import { useRoutes } from "react-router-dom";
+
+/*Routes is used to be Switch*/
+const Router = () => {
+  /* nesting routes*/
+  /*return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );*/
+
+  // object-based routes
+  return useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "games", element: <Painel /> },
+    { path: "*", element: <NotFound /> },
+  ]);
+};
+export default Router;
