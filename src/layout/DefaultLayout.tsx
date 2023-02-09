37cfe96ae13a91edcdc5dcf14a5be20d8b7589e7
Glaucio Almeida <glaucio@mail.com>
2023-02-08 21:02:51 -0300

    layout up

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,23 +1,42 @@
 import { CssBaseline } from "@mui/material";
 import Navbar from "../components/Navbar";
 
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  createTheme,
+  PaletteColorOptions,
+  ThemeProvider,
+} from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface CustomPalette {
+    blue: PaletteColorOptions;
+    black: PaletteColorOptions;
+  }
+  interface Palette extends CustomPalette {}
+  interface PaletteOptions extends CustomPalette {}
+}
+
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor: any) =>
+  augmentColor({ color: { main: mainColor } });
 
 const theme = createTheme({
   palette: {
     background: {
       default: "#b1b1b1",
     },
+    blue: createColor("#0bb2f4"),
+    black: createColor("#000000"),
     primary: {
-      light: "#757ce8",
+      light: "#002884",
       main: "#ffffff",
-      dark: "#002884",
+      dark: "#000000",
       contrastText: "#000",
     },
     secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
+      main: "#727272",
+      dark: "#000",
       contrastText: "#000",
     },
   },
